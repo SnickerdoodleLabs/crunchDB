42e69b1d9fa7b706f72978403cdd75bced2cda7a
Enes Dolgun <58950702+dlgn2@users.noreply.github.com>
2024-04-17 18:27:32 +0300

    Update IPersistenceConfig.ts

--- src/implementations/business/volatile/IPersistenceConfig.ts
+++ src/implementations/business/volatile/IPersistenceConfig.ts
@@ -1,17 +1,3 @@
-import { URLString } from '@crunchDB/objects';
-
 export interface IPersistenceConfig {
-  accountBalancePollingIntervalMS: number;
-  accountNFTPollingIntervalMS: number;
-  backupChunkSizeTarget: number;
-  defaultInsightPlatformBaseUrl: URLString;
-  defaultGoogleCloudBucket: string;
-  restoreTimeoutMS: number;
-  enableBackupEncryption: boolean;
-  dataWalletBackupIntervalMS: number;
-  ipfsFetchBaseUrl: URLString;
-
-  dropboxAppKey: string;
-  dropboxAppSecret: string;
-  dropboxRedirectUri: string;
+  // Configs
 }
