c5ceb82aee0564ef9e14c082831637b79e241967
Adhocmaster <adhocmaster@live.com>
2024-04-23 16:36:49 -0700

    ok, from jest to test

--- test/wasm/Eigen.test.ts
+++ test/wasm/Eigen.test.ts
@@ -80,22 +80,27 @@ describe('Eigen', () => {
     it('should add two matrices', async () => {
         const mat1 = Float32Array.from([1, 2, 3, 4]);
         const mat2 = Float32Array.from([5, 6, 7, 8]);
+        const expected = Float32Array.from([6, 8, 10, 12]);
         const api =  await eigenFactory().then((eigen: any) => {
             return makeAPI(eigen);
         });
         // console.log('api', api);
         let mat1ptr = vector2Float32Ptr(api, mat1);
-        console.log('mat1ptr', mat1ptr);
-        console.log('mat1ptr to array again', float32Ptr2Vector(api, mat1ptr, 4));
+        // console.log('mat1ptr', mat1ptr);
+        // console.log('mat1ptr to array again', float32Ptr2Vector(api, mat1ptr, 4));
         let mat2ptr = vector2Float32Ptr(api, mat2);
-        console.log('mat2ptr', mat2ptr);
-        console.log('mat2ptr to array again', float32Ptr2Vector(api, mat2ptr, 4));
+        // console.log('mat2ptr', mat2ptr);
+        // console.log('mat2ptr to array again', float32Ptr2Vector(api, mat2ptr, 4));
         expect(mat1).toEqual(float32Ptr2Vector(api, mat1ptr, 4));
 
         // console.log(api);
-        // let result = Float32Array.from({ length: 4 });
-        // api.add(1, 1, 1, 4, mat1, mat2, result);
+        let resultPtr =  vector2Float32Ptr(api, Float32Array.from({ length: 4 }));
+
+        api.add(1, 1, 1, 4, mat1ptr, mat2ptr, resultPtr);
+        const result = float32Ptr2Vector(api, resultPtr, 4);
+        // console.log('result', result);
         // const result = eigen.float_matrix_matrix_add(1, 1, 1, 4, mat1, mat2);
         // console.log("result", result);
+        expect(result).toEqual(expected);
     });
 });
