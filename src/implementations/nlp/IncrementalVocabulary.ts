f82a29fedad3b8ddb08f0cd64cae53fd55798fbd
Ayush Ranjan <ayushranjancr7@gmail.com>
2024-04-23 23:33:18 -0700

    Incremental Vocabulary Implementation

--- src/implementations/nlp/IncrementalVocabulary.ts
+++ src/implementations/nlp/IncrementalVocabulary.ts
@@ -1,12 +1,22 @@
-import { IIncrementalVocabulary } from "interfaces";
-import { Word, WordRoot, VocabularySize, WordIndex } from "objects";
-import { WordToIndex } from "objects/business/nlp/WordToIndex";
+import { Word, WordRoot, WordIndex } from "crunchDB/objects";
+import { IIncrementalVocabulary } from "crunchDB/interfaces"; 
 import { Vocabulary } from "./Vocabulary";
+import {  Err } from 'neverthrow'; 
 
 export class IncrementalVocabulary extends Vocabulary implements IIncrementalVocabulary {
-    public addWord(word: Word | WordRoot): void {
-        this.wordToIndex[word] = WordIndex(this.indexToWord.length);
-        this.indexToWord.push(word);
-    }
 
+  /*
+  * @param words: an array of words/ word roots
+  * @description: adds words/ word roots to the vocabulary
+  * @throws Err if the word/ word root is already present in the vocabulary
+  * */
+  public addWord(word: Word | WordRoot): void {
+    if (this.wordToIndex.hasOwnProperty(word)) {
+      
+      throw new Err(`Duplicate word "${word}" cannot be added.`);
+    }
+    this.wordToIndex[word] = WordIndex(this.indexToWord.length);
+    this.indexToWord.push(word);
+  }
 }
+
--- src/implementations/nlp/index.ts
+++ src/implementations/nlp/index.ts
@@ -1,1 +1,2 @@
 export * from './Vocabulary';
+export * from './IncrementalVocabulary';
