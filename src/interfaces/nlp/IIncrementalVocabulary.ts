1ee383076e700b88c444b2d1f24bbae98aedd10b
Adhocmaster <adhocmaster@live.com>
2024-04-26 09:43:29 -0700

    Update IIncrementalVocabulary.ts

--- src/interfaces/nlp/IIncrementalVocabulary.ts
+++ src/interfaces/nlp/IIncrementalVocabulary.ts
@@ -1,7 +1,8 @@
 import { Word, WordRoot } from "crunchDB/objects";
 import { IVocabulary } from "./IVocabulary";
+import { ResultAsync } from "neverthrow";
 
 export interface IIncrementalVocabulary extends IVocabulary {
-    addWord(word: Word | WordRoot): void;
+    addWord(word: Word | WordRoot): ResultAsync<void, never>;
 }
 export const IIncrementalVocabularyType = Symbol.for("IIncrementalVocabulary");
