5768fed1f8a85cb7af1171964224e5b836a8d22a
Adhocmaster <adhocmaster@live.com>
2024-04-30 10:03:07 -0700

    Update SampleSimilarity.test.ts

--- test/implementations/ranking/SampleSimilarity.test.ts
+++ test/implementations/ranking/SampleSimilarity.test.ts
@@ -6,8 +6,8 @@ describe("SampleSimilarity", () => {
     it("should get ok", async () => {
         // Arrange
         const measure = new SampleSimilarity();
-        const embedding1 = new Embedding([1, 1, 1]);
-        const embedding2 = new Embedding([1, 1, 1]);
+        const embedding1 = Embedding([1, 1, 1]);
+        const embedding2 = Embedding([1, 1, 1]);
         const expected = SimilarityScoreNormalized(0.05);
 
         // Act
@@ -22,8 +22,8 @@ describe("SampleSimilarity", () => {
     it("should get error", async () => {
         // Arrange
         const measure = new SampleSimilarity();
-        const embedding1 = new Embedding([1, 1, 1]);
-        const embedding2 = new Embedding([1, 1, 10]);
+        const embedding1 = Embedding([1, 1, 1]);
+        const embedding2 = Embedding([1, 1, 10]);
 
         // Act
         const result = await measure.getNormalizedSimilarityScore(embedding1, embedding2);
