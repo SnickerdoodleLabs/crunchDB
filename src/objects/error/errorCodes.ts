ca6ca313ac04d47b936324cd5ac35ef0d8992169
Adhocmaster <adhocmaster@live.com>
2024-05-06 12:26:12 -0700

    removed unnecessary errors

--- src/objects/error/errorCodes.ts
+++ src/objects/error/errorCodes.ts
@@ -1,7 +1,6 @@
 const errorCodes: {[Key: string]: string} = {
         DataError: "ERR_DataError",
     IndexDBError: "ERR_IndexDBError",
-    IndexDBError2: "ERR_IndexDBError2",
     NLPError: "ERR_NLPError",
     DuplicateWordError: "ERR_DuplicateWordError",
     EigenJSNotReadyError: "ERR_EigenJSNotReadyError",
